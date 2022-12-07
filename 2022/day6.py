b5a6c219c25975fda530db5d39f2e301a801f7b9
dmaljovec <daniel.maljovec@recursionpharma.com>
2022-12-07 00:12:32 -0700

    lint

--- 2022/day6.py
+++ 2022/day6.py
@@ -2,6 +2,7 @@ from aocd import get_data, submit
 
 datastream = get_data(day=6, year=2022)
 
+
 def get_first_unique(datastream, unique_size):
     marker = []
     for i, character in enumerate(datastream, start=1):
@@ -10,6 +11,7 @@ def get_first_unique(datastream, unique_size):
             return i
         if len(marker) == unique_size:
             marker.pop(0)
+
 
 submit(get_first_unique(datastream, 4), part="a", day=6, year=2022)
 submit(get_first_unique(datastream, 14), part="b", day=6, year=2022)
