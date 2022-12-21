ed9daf89dd4908f4f28d294e5bcbbcbf4d2fb6a0
dmaljovec <daniel.maljovec@recursionpharma.com>
2022-12-21 00:26:46 -0700

    ugly day 16 solution (took a day to run I think)

--- 2022/day16.py
+++ 2022/day16.py
@@ -12,18 +12,6 @@ VALVES = {}
 TUNNELS = {}
 
 data = get_data(day=DAY, year=YEAR).split("\n")
-data = [
-    "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
-    "Valve BB has flow rate=13; tunnels lead to valves CC, AA",
-    "Valve CC has flow rate=2; tunnels lead to valves DD, BB",
-    "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
-    "Valve EE has flow rate=3; tunnels lead to valves FF, DD",
-    "Valve FF has flow rate=0; tunnels lead to valves EE, GG",
-    "Valve GG has flow rate=0; tunnels lead to valves FF, HH",
-    "Valve HH has flow rate=22; tunnel leads to valve GG",
-    "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
-    "Valve JJ has flow rate=21; tunnel leads to valve II",
-]
 for line in data:
     # :sadtrombone:
     # "re module doesn't support repeated captures" - https://stackoverflow.com/a/9765390/11580262
@@ -34,6 +22,11 @@ for line in data:
     valve, rate, _, _, _, edges = match.groups()
     VALVES[valve] = int(rate)
     TUNNELS[valve] = [e for e in edges.split(", ")]
+
+
+def print_path(path):
+    for p in path:
+        print(f"{p[0]} w/ {p[1]} minutes left and closed: {p[2]} ({p[3]})")
 
 
 ################################################################################
@@ -73,6 +66,7 @@ starting_state = set(VALVES.keys()) - set(
 
 ################################################################################
 ## Part B
+CALL_COUNT = 0
 
 
 @cache
@@ -82,6 +76,11 @@ def optimal_path_with_elephant(
     off: tuple[str],
 ) -> int:
     off = set(off)
+    global CALL_COUNT
+    CALL_COUNT += 1
+    if CALL_COUNT % 100000 == 0:
+        print(CALL_COUNT, off)
+
     # Base case, we are either out of time or have turned on everything already
     if time_remaining < 2 or len(off) == 0:
         return 0
@@ -89,24 +88,26 @@ def optimal_path_with_elephant(
     max_pressure_released = 0
     if locations[0] in off:
         # Case: We both turn our valves
-        if locations[1] in off:
+        if locations[1] in off and locations[0] != locations[1]:
             potential_pressure = VALVES[locations[0]] * (time_remaining - 1)
             potential_pressure += VALVES[locations[1]] * (time_remaining - 1)
+            new_off = tuple(sorted(list(off - set(locations))))
             potential_pressure += optimal_path_with_elephant(
                 locations,
                 time_remaining - 1,
-                tuple(off - set(locations)),
+                new_off,
             )
             max_pressure_released = max(max_pressure_released, potential_pressure)
 
         # Case: I turn my valve off, Ellie moves
         for valve in TUNNELS[locations[1]]:
             potential_pressure = VALVES[locations[0]] * (time_remaining - 1)
+            new_off = tuple(sorted(list(off - set(locations[:1]))))
             potential_pressure += optimal_path_with_elephant(
                 # Improve caching since who is where is not important
                 tuple(sorted([locations[0], valve])),
                 time_remaining - 1,
-                tuple(off - set(locations[0])),
+                new_off,
             )
             max_pressure_released = max(max_pressure_released, potential_pressure)
 
@@ -114,21 +115,23 @@ def optimal_path_with_elephant(
         # Case: I move, Ellie turns her valve
         if locations[1] in off:
             potential_pressure = VALVES[locations[1]] * (time_remaining - 1)
+            new_off = tuple(sorted(list(off - set(locations[1:]))))
             potential_pressure += optimal_path_with_elephant(
                 # Improve caching since who is where is not important
                 tuple(sorted([my_move, locations[1]])),
                 time_remaining - 1,
-                tuple(off - set(locations[1])),
+                new_off,
             )
             max_pressure_released = max(max_pressure_released, potential_pressure)
 
         # Case: I move, Ellie moves
         for ellies_move in TUNNELS[locations[1]]:
+            new_off = tuple(sorted(list(off)))
             potential_pressure = optimal_path_with_elephant(
                 # Improve caching since who is where is not important
                 tuple(sorted([my_move, ellies_move])),
                 time_remaining - 1,
-                tuple(off),
+                new_off,
             )
             max_pressure_released = max(max_pressure_released, potential_pressure)
 
@@ -137,5 +140,11 @@ def optimal_path_with_elephant(
 
 tag_team = optimal_path_with_elephant(("AA", "AA"), 26, tuple(starting_state))
 print(tag_team)
+print(CALL_COUNT)
 
-# submit(optimal_path_with_elephant("AA", "AA", 26, tuple(starting_state)), part="b", day=DAY, year=YEAR)
+submit(
+    optimal_path_with_elephant(("AA", "AA"), 26, tuple(starting_state)),
+    part="b",
+    day=DAY,
+    year=YEAR,
+)
