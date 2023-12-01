a862cb4f9d990df477ba644334b71339f1b771c0
dmaljovec <daniel.maljovec@recursionpharma.com>
2023-12-01 14:41:45 -0700

    chore: add input and boilerplate for day 1 with my friends

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+fn main() -> i32 {
     println!("Hello, world!");
+    5
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        assert_eq!(main(), 5);
+    }
 }
