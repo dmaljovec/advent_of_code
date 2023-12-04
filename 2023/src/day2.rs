be560001767a711045e3f4f72afeb00258862917
dmaljovec <daniel.maljovec@recursionpharma.com>
2023-12-04 13:08:14 -0700

    add test data for day 2

--- 2023/src/day2.rs
+++ 2023/src/day2.rs
@@ -1,5 +1,9 @@
 use regex::Regex;
 
+const RED_COUNT = 12;
+const GREEN_COUNT = 13;
+const BLUE_COUNT = 14;
+
 pub fn solution_pt1(input: &str) -> i32 {
     0
 }
@@ -8,13 +12,31 @@ pub fn solution_pt2(input: &str) -> i32 {
     0
 }
 
+fn parse_line(line: &str) -> Vec<(i32,i32,i32)> {
+    let mut max_red = 0;
+    let mut max_green = 0;
+    let mut max_blue = 0;
+    let handfuls = line.split(":")[1].split(";");
+
+    (max_red, max_green, max_blue)
+}
+
+fn parse_handful(handful: &str) -> (i32,i32,i32) {
+    let mut red = 0;
+    let mut green = 0;
+    let mut blue = 0;
+    let colors = handful.split(",");
+
+    (red, green, blue)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_solution() {
-        let input = "day1/part_1.txt";
-        assert_eq!(solution_pt1(input), 0);
+    fn test_parse_handful() {
+        let input = "12 blue, 15 red, 2 green";
+        assert_eq!(parse_handful(input), (15, 2, 12));
     }
 }
