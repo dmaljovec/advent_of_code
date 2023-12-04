fb3535364b4e6be3977c03b142fd0af55d266a50
dmaljovec <daniel.maljovec@recursionpharma.com>
2023-12-04 12:53:08 -0700

    setup for multi-day adventing

---
+++ 2023/src/day2.rs
@@ -0,0 +1,20 @@
+use regex::Regex;
+
+pub fn solution_pt1(input: &str) -> i32 {
+    0
+}
+
+pub fn solution_pt2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        let input = "day1/part_1.txt";
+        assert_eq!(solution_pt1(input), 0);
+    }
+}
